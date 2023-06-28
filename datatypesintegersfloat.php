1e6ca5cc90c98ef6c852a52b8d35fa1c6331ed80
kskannandotwin <kskannan.win@gmail.com>
2023-06-28 19:08:24 +0530

    28.06.2023-dtiaf
    completed

---
+++ datatypesintegersfloat.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>data types: integers and floats</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <style>
+       h1 {
+           color: purple;
+       }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+       <h1>Integers and Floats</h1>
+       <div>
+        <?php 
+        $x = 7;
+        var_dump($x);
+        $y = 3;
+        $z = $x + $y;
+        echo '<br>';
+        echo $z;
+        $z = 0x1A;
+        echo '<br>';
+        var_dump($z);
+        $w = 0123;
+        echo '<br>';
+        var_dump($w);
+        
+        $floatingNumber = 3.7;
+        echo '<br>';
+        var_dump($floatingNumber);
+        ?>
+       </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
