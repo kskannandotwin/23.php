9f8432705795e6bf2474d727d5b1607c7cc5f145
kskannandotwin <kskannan.win@gmail.com>
2023-06-30 06:58:01 +0530

    30.06.2023-sf
    completed

---
+++ stringfunctions.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>String functions</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <style>
+       h1 {
+           color: purple;
+       }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+       <h1>String functions</h1>
+       <div>
+        <?php
+        $message = 'Welcome to my website! Have a good time exploring my website!';
+        echo 'Length of my message ' . strlen($message);
+        echo '<br>';
+        echo 'My message contains ' . str_word_count($message) . ' words.';
+        echo '<br>';
+        echo str_replace('website', 'world', $message, $number_of_words_replaced);
+        echo '<br>';
+        echo $message;
+        echo '<br>';
+        echo 'Number of words replaced ' . $number_of_words_replaced . ' words.';
+        echo '<br>';
+        echo 'Can you read from the right?<br>' . strrev($message);
+        ?>
+       </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
