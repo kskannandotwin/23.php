8fe90140805281c209804cb3629307e13e22f09f
kskannandotwin <kskannan.win@gmail.com>
2023-07-06 06:37:23 +0530

    06.07.2023-ipf
    completed

---
+++ footer.php
@@ -0,0 +1,30 @@
+<style>
+
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+body {
+    margin-bottom: 60px;
+}
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    background-color: #f3f1f1;
+    height: 50px;
+}
+</style>
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">
+        <?php 
+        $today = date('Y');
+        echo "Copyright &copy; 2010-$today."
+        ?>
+        </p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,33 @@
+    <nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">Store</a>
+                <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse" id="navbarCollapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">All Products</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Departments<span class="caret"></span></a></li>
+                </ul>
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-info">Go</button>
+                        </span>
+                        <label for="search" class="sr-only">Search</label>
+                        <input type="text" class="form-control" id="search" placeholder="Search">
+                        <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                    </div>
+                </form>
+                <form class="navbar-form navbar-right">
+                    <input type="button" class="btn btn-success" value="Login">
+                </form>
+            </div>
+        </div>
+    </nav>
